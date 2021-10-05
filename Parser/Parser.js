2f39106b53380f2ff72bf83a85e28dea6ea2a2e9
NoroIsakhanyan <78563222+NoroIsakhanyan@users.noreply.github.com>
2021-10-05 10:31:13 +0400

    Delete Parser directory

--- Parser/Parser.js
+++
@@ -1,7 +0,0 @@
-const SaveData = require('./saver/SaveData');
-const parser = require('./parser/parser');
-
-module.exports = async function Parse(MAIN_URL){
-    const result = await parser(MAIN_URL);
-    SaveData(result.PageTitle, result.MainPageURL, result.PageContent);
-}
